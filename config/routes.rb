0f9a189b059befc80e3421b94a02314fe348bd13
Daniel Fermanian <dfermanian@gmail.com>
2010-09-02 12:11:57 -0700

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :hellos
+  map.resources :donations, :new => { :create_express => :get }
 
+   map.resources :brides
+
+   map.resources :gifts
+
+#   map.root :controller => :brides, :action => :index
   # Tell Rails to make invitations into a resource
   map.resources :invitations
   # Use our new method as the default page
