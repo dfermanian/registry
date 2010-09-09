9009d935321b858640cdad7a684edc30f5c34f32
Daniel Fermanian <dfermanian@gmail.com>
2010-09-09 11:01:24 -0700

    changed USerMailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < Facebooker::Rails::Publisher
   
   def profile(user)
-      send_as :profile
-      from user
-      recipients user
       fbml = "This is some test FBML that will be inserted into a user is profile."
-      profile(fbml)
-      profile_main(fbml)
+      send_as :publish_stream
+      from user
+      target user
+      message fbml
     end
     
 end
