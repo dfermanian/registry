48a63f06ffded04dd2fe12a09d3d9f6e4ac587ce
Daniel Fermanian <dfermanian@gmail.com>
2010-09-16 14:08:00 -0700

    added action linkS

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < Facebooker::Rails::Publisher
       from from_user
       target to_user
       message message_fbml
-      action_link("Add the app now!", "http://www.apps.facebook.com/ptndlol")
+      action_links action_link("Add the app now!", "http://www.apps.facebook.com/ptndlol")
     end
     
 
